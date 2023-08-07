a078cf1b95ba12cf1180c064d713e8cd3dec737e
SethuSrivatsa8 <137143925+SethuSrivatsa8@users.noreply.github.com>
2023-08-07 19:32:45 +0530

    Time: 926 ms (24.61%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,10 +1,7 @@
 # Write your MySQL query statement below
 SELECT 
-    CASE
-        WHEN MOD(id, 2) = 1 AND id < (SELECT MAX(id) FROM Seat) THEN id + 1
-        WHEN MOD(id, 2) = 0 THEN id - 1
-        ELSE id
-    END AS id,
+    IF(MOD(id, 2) = 1 AND id < (SELECT MAX(id) FROM Seat), id + 1,
+       IF(MOD(id, 2) = 0, id - 1, id)) AS id,
     student
 FROM Seat
 ORDER BY id;
